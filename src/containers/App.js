2329a02b4b030557baf46329acac70fa61c21daf
David Athobe <dathobek@gmail.com>
2020-09-30 15:59:00 +0100

    destructuring

--- src/containers/App.js
+++ src/containers/App.js
@@ -27,10 +27,11 @@ onSearchChange = (event)=>{
 }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        const {robots,searchField} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
-        if(this.state.robots.length === 0) {
+        if(!robots.length) {
            return <h1>LOADING</h1>
         } else {
             return(
